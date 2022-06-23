44657ead233de4ffcf20bc0cfb246ef05b1f82ae
Danila <dan_crysis@mail.ru>
2022-06-23 21:21:51 +0300

    DZ-2

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿NewMethod();
+
+static void NewMethod()
+{
+    //Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+    int Number = Convert.ToInt32(Console.ReadLine());
+
+    if (Number == 1)
+    {
+        Console.WriteLine("Понедельник");
+    }
+    if (Number == 2)
+    {
+        Console.WriteLine("Вторник");
+    }
+
+if (Number == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+
+if (Number == 4)
+    {
+        Console.WriteLine("Четверг");
+    }
+
+if (Number == 5)
+    {
+        Console.WriteLine("Пятница");
+    }
+    if (Number == 6)
+    {
+        Console.WriteLine("Суббота");
+    }
+
+if (Number == 7)
+    {
+        Console.WriteLine("Воскресение");
+    }
+
+
+
+}
